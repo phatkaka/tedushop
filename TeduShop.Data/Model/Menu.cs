6e778cbe06d80d8a3ded4982fcdc7ba1690bdfce
Your Name <you@example.com>
2016-07-19 04:44:33 +0700

    first

---
+++ TeduShop.Data/Model/Menu.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Data.Model
+{
+    [Table("Menus")]
+    public class Menu
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] 
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Url { get; set; }
+        public int? DislpayOrder { get; set; }
+        [Required]
+        public int GroupId { get; set; }
+        [ForeignKey("GroupId")]
+        public virtual MenuGroup MenuGroups { set; get; }
+        public string Target { get; set; }
+        [Required]
+        public bool Status { get; set; }
+
+    }
+}
